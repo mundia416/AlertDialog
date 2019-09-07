fc002f538e3b6e75b2a32f109ce09d3eff3f1505
Mundia <fxmundia416@gmail.com>
2019-09-07 14:04:09 +0200

    updated the alert dialog

--- alertlib/src/main/java/com/nosetrap/alertlib/AlertDialog.kt
+++ alertlib/src/main/java/com/nosetrap/alertlib/AlertDialog.kt
@@ -9,33 +9,23 @@ import android.view.Gravity
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import androidx.annotation.IdRes
+import androidx.annotation.ColorRes
 import androidx.annotation.LayoutRes
-import cn.pedant.SweetAlert.SweetAlertDialog
-import kotlinx.android.synthetic.main.alert_ok.*
+import kotlinx.android.synthetic.main.ok_alert_view.*
+import kotlinx.android.synthetic.main.alert_view.*
 import kotlinx.android.synthetic.main.body_view.*
-import kotlinx.android.synthetic.main.confirm_alert.*
+import kotlinx.android.synthetic.main.confirm_alert_view.*
 
 class AlertDialog(private val activity: Activity,private val alertType: Int = TYPE_NORMAL) {
 
     companion object{
+
         /**
          * the type of alert dialog that is used
          */
         const val TYPE_NORMAL = 0
         const val TYPE_OK = 1
         const val TYPE_CONFIRM = 2
-
-        /**
-         * determines what type of custom view is used
-         */
-        //the custom view is disabled so use the default view
-        private const val CUSTOM_VIEW_DISABLED = 0
-        // a layout resource id is the custom view
-        private const val CUSTOM_VIEW_LAYOUT_RES = 1
-        // a view object is the custom view
-        private const val CUSTOM_VIEW_OBJECT = 2
-
     }
 
 
@@ -45,37 +35,30 @@ class AlertDialog(private val activity: Activity,private val alertType: Int = TY
     var onConfirmClicked: (() -> Unit)? = null
     var onCancelClicked: (() -> Unit)? = null
 
-    private var view: Any? = null
-    private var customViewType: Int = CUSTOM_VIEW_DISABLED
+    var titleText : String = ""
+    var contentText: String = ""
+
+    @LayoutRes private var view: Int = R.layout.body_view
+
+    @ColorRes private var lineColor: Int = R.color.sky_blue
+
+    private val layoutInflater = activity.layoutInflater
 
     fun setCustomView(@LayoutRes view: Int){
         this.view = view
-        customViewType = CUSTOM_VIEW_LAYOUT_RES
     }
 
-    fun setCustomView(view: View){
-        this.view = view
-        customViewType = CUSTOM_VIEW_OBJECT
-    }
+
 
     fun dismiss(){
-        SweetAlertDialog.NORMAL_TYPE
         dialog.dismiss()
     }
 
-    private fun setDialogView() {
-        when (customViewType) {
-            CUSTOM_VIEW_OBJECT -> dialog.setContentView(view as View)
-            CUSTOM_VIEW_LAYOUT_RES -> dialog.setContentView(view as Int)
-            else -> dialog.setContentView(getDialogView())
-        }
-    }
-    
-    fun show(title: String, message: String) {
+    fun show() {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        setDialogView()
+        setAlertView()
 
         val dm = DisplayMetrics()
         activity.windowManager.defaultDisplay.getMetrics(dm)
@@ -90,28 +73,50 @@ class AlertDialog(private val activity: Activity,private val alertType: Int = TY
         lp.windowAnimations = R.style.DialogAnimation
         window.attributes = lp
 
-        dialog.tvTitle.text = title
-        dialog.tvMessage.text = message
+        dialog.tvTitle.text = titleText
+        dialog.tvMessage.text = contentText
 
         setOnClickListeners()
 
         dialog.show()
     }
 
+    private fun setAlertView(){
+        dialog.setContentView(R.layout.alert_view)
+
+        val addView = layoutInflater.inflate(view,null,false)
+        dialog.contentContainer.addView(addView)
+
+        when(alertType){
+            TYPE_OK -> {
+                dialog.buttonsContainer.addView(layoutInflater.inflate(R.layout.ok_alert_view,null,false))
+            }
+            TYPE_CONFIRM -> {
+                dialog.buttonsContainer.addView(layoutInflater.inflate(R.layout.confirm_alert_view,null,false))
+            }
+            TYPE_NORMAL -> {
+                dialog.horizontalLine.visibility = View.GONE
+            }
+        }
+
+        dialog.horizontalLine.setBackgroundColor(lineColor)
+
+    }
+
     private fun setOnClickListeners(){
         when(alertType){
             TYPE_OK -> {
-                dialog.tvOk.setOnClickListener {
+                dialog.btnOk.setOnClickListener {
                     dialog.dismiss()
                     onOkClicked?.invoke()
                 }
             }
             TYPE_CONFIRM ->{
-                dialog.tvConfirm.setOnClickListener {
+                dialog.btnConfirm.setOnClickListener {
                     dialog.dismiss()
                     onConfirmClicked?.invoke()
                 }
-                dialog.tvCancel.setOnClickListener {
+                dialog.btnCancel.setOnClickListener {
                     dialog.dismiss()
                     onCancelClicked?.invoke()
                 }
@@ -119,11 +124,7 @@ class AlertDialog(private val activity: Activity,private val alertType: Int = TY
         }
     }
 
-    private fun getDialogView(): Int{
-        return when(alertType){
-            TYPE_CONFIRM -> R.layout.confirm_alert
-            TYPE_OK -> R.layout.alert_ok
-            else -> R.layout.alert_normal
-        }
+    fun setLineColor(@ColorRes color: Int){
+        this.lineColor = color
     }
 }

fcd1a4f7ae1afe89ed2e1b043746fff1f53cb258
Mundia <fxmundia416@gmail.com>
2019-09-08 11:05:53 +0200

    bug fix

--- alertlib/src/main/java/com/nosetrap/alertlib/AlertDialog.kt
+++ alertlib/src/main/java/com/nosetrap/alertlib/AlertDialog.kt
@@ -17,115 +17,137 @@ import kotlinx.android.synthetic.main.alert_view.*
 import kotlinx.android.synthetic.main.body_view.*
 import kotlinx.android.synthetic.main.confirm_alert_view.*
 
-class AlertDialog(private val activity: Activity,private val alertType: Int = TYPE_NORMAL) {
-
-    companion object{
-
-        /**
-         * the type of alert dialog that is used
-         */
-        const val TYPE_NORMAL = 0
-        const val TYPE_OK = 1
-        const val TYPE_CONFIRM = 2
-    }
-
+/**
+ *
+ *  make sure to call [build] before calling [show]
+*
+ */
+class AlertDialog private constructor(private val activity: Activity) {
 
     val dialog = Dialog(activity)
 
-    var onOkClicked: (() -> Unit)? = null
-    var onConfirmClicked: (() -> Unit)? = null
-    var onCancelClicked: (() -> Unit)? = null
-
-    var titleText : String = ""
-    var contentText: String = ""
-
-    @LayoutRes private var view: Int = R.layout.body_view
-
-    @ColorRes private var lineColor: Int = R.color.sky_blue
-
-    private val layoutInflater = activity.layoutInflater
-
-    fun setCustomView(@LayoutRes view: Int){
-        this.view = view
-    }
-
-
-
-    fun dismiss(){
+    fun dismiss() {
         dialog.dismiss()
     }
 
     fun show() {
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        setAlertView()
-
-        val dm = DisplayMetrics()
-        activity.windowManager.defaultDisplay.getMetrics(dm)
-        val lp = WindowManager.LayoutParams()
-        val window = dialog.window
-        window!!.setGravity(Gravity.CENTER)
-        lp.copyFrom(window.attributes)
-
-        //This makes the dialog take up the full width
-        lp.width = WindowManager.LayoutParams.MATCH_PARENT
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT
-        lp.windowAnimations = R.style.DialogAnimation
-        window.attributes = lp
-
-        dialog.tvTitle.text = titleText
-        dialog.tvMessage.text = contentText
-
-        setOnClickListeners()
-
         dialog.show()
     }
 
-    private fun setAlertView(){
-        dialog.setContentView(R.layout.alert_view)
 
-        val addView = layoutInflater.inflate(view,null,false)
-        dialog.contentContainer.addView(addView)
+    /**
+     * use the builder class to create an alert dialog
+     */
+    class Builder(private val activity: Activity, private val alertType: AlertType = AlertType.TYPE_NORMAL) {
 
-        when(alertType){
-            TYPE_OK -> {
-                dialog.buttonsContainer.addView(layoutInflater.inflate(R.layout.ok_alert_view,null,false))
+        var onOkClicked: (() -> Unit)? = null
+        var onConfirmClicked: (() -> Unit)? = null
+        var onCancelClicked: (() -> Unit)? = null
+
+        var titleText: String = ""
+        var contentText: String = ""
+
+        @LayoutRes
+        private var view: Int = R.layout.body_view
+
+        @ColorRes
+        private var lineColor: Int = R.color.sky_blue
+
+        private val layoutInflater = activity.layoutInflater
+
+        val alertDialog = AlertDialog(activity)
+
+        fun setCustomView(@LayoutRes view: Int) {
+            this.view = view
+        }
+
+        /**
+         *  call [build] before calling [show]
+         */
+        fun build(): AlertDialog {
+            alertDialog.dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            alertDialog.dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            setAlertView()
+
+            val dm = DisplayMetrics()
+            activity.windowManager.defaultDisplay.getMetrics(dm)
+            val lp = WindowManager.LayoutParams()
+            val window = alertDialog.dialog.window
+            window!!.setGravity(Gravity.CENTER)
+            lp.copyFrom(window.attributes)
+
+            //This makes the dialog take up the full width
+            lp.width = WindowManager.LayoutParams.MATCH_PARENT
+            lp.height = WindowManager.LayoutParams.WRAP_CONTENT
+            lp.windowAnimations = R.style.DialogAnimation
+            window.attributes = lp
+
+            alertDialog.dialog.tvTitle.text = titleText
+            alertDialog.dialog.tvMessage.text = contentText
+
+            setOnClickListeners()
+
+            return alertDialog
+        }
+
+        private fun setAlertView() {
+            alertDialog.dialog.setContentView(R.layout.alert_view)
+
+            val addView = layoutInflater.inflate(view, null, false)
+            alertDialog.dialog.contentContainer.addView(addView)
+
+            when (alertType) {
+                AlertType.TYPE_OK -> {
+                    alertDialog.dialog.buttonsContainer.addView(
+                        layoutInflater.inflate(
+                            R.layout.ok_alert_view,
+                            null,
+                            false
+                        )
+                    )
+                }
+                AlertType.TYPE_CONFIRM -> {
+                    alertDialog.dialog.buttonsContainer.addView(
+                        layoutInflater.inflate(
+                            R.layout.confirm_alert_view,
+                            null,
+                            false
+                        )
+                    )
+                }
+                AlertType.TYPE_NORMAL -> {
+                    alertDialog.dialog.horizontalLine.visibility = View.GONE
+                }
             }
-            TYPE_CONFIRM -> {
-                dialog.buttonsContainer.addView(layoutInflater.inflate(R.layout.confirm_alert_view,null,false))
-            }
-            TYPE_NORMAL -> {
-                dialog.horizontalLine.visibility = View.GONE
+
+            alertDialog.dialog.horizontalLine.setBackgroundColor(ContextCompat.getColor(activity!!, lineColor))
+
+        }
+
+        private fun setOnClickListeners() {
+            when (alertType) {
+                AlertType.TYPE_OK -> {
+                    alertDialog.dialog.btnOk.setOnClickListener {
+                        alertDialog.dialog.dismiss()
+                        onOkClicked?.invoke()
+                    }
+                }
+                AlertType.TYPE_CONFIRM -> {
+                    alertDialog.dialog.btnConfirm.setOnClickListener {
+                        alertDialog.dialog.dismiss()
+                        onConfirmClicked?.invoke()
+                    }
+                    alertDialog.dialog.btnCancel.setOnClickListener {
+                        alertDialog.dialog.dismiss()
+                        onCancelClicked?.invoke()
+                    }
+                }
             }
         }
 
-        dialog.horizontalLine.setBackgroundColor(ContextCompat.getColor(activity!!,lineColor))
-
-    }
-
-    private fun setOnClickListeners(){
-        when(alertType){
-            TYPE_OK -> {
-                dialog.btnOk.setOnClickListener {
-                    dialog.dismiss()
-                    onOkClicked?.invoke()
-                }
-            }
-            TYPE_CONFIRM ->{
-                dialog.btnConfirm.setOnClickListener {
-                    dialog.dismiss()
-                    onConfirmClicked?.invoke()
-                }
-                dialog.btnCancel.setOnClickListener {
-                    dialog.dismiss()
-                    onCancelClicked?.invoke()
-                }
-            }
+        fun setLineColor(@ColorRes color: Int) {
+            this.lineColor = color
         }
-    }
-
-    fun setLineColor(@ColorRes color: Int){
-        this.lineColor = color
     }
 }
---
+++ alertlib/src/main/java/com/nosetrap/alertlib/AlertType.kt
@@ -0,0 +1,7 @@
+package com.nosetrap.alertlib
+
+enum class AlertType {
+    TYPE_NORMAL,
+     TYPE_OK ,
+     TYPE_CONFIRM
+}
